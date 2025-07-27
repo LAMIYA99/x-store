18ba20656f2a1537c75c68291cd1e9dac673925c
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-07-27 18:12:02 +0400

    all changes in homepage added

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {});
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+  
