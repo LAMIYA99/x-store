32181ede013d76d8eea2361f939a1548a75bd32f
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-07-29 17:15:52 +0400

    fix:swiperjs

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,32 @@
 var swiper = new Swiper(".mySwiper", {});
 
+var swiper = new Swiper(".mySwipper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+  }, breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+
+});
+
 window.addEventListener('scroll', () => {
-    const header = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
-  
+  const header = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
