46dbf0d62ab7466f28afef43b057131257084589
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-10-06 20:55:44 +0400

    done

---
+++ Desktop/kristal-task-react/kristal-task/src/Featured/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeTemplates from '../Templates/HomeTemplates'
+
+const Home = () => {
+  return (
+    <div><HomeTemplates/></div>
+  )
+}
+
+export default Home
---
+++ Desktop/kristal-task-react/kristal-task/src/Featured/Templates/HomeTemplates.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeTemplates = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default HomeTemplates
